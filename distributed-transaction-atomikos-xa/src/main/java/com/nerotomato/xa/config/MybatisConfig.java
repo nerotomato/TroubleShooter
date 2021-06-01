1d0a13f14a97297eae88be0a02ffdebaaa90153e
tomato <guanhaowen@outlook.com>
2021-06-01 10:23:06 +0800

    1.基于shardingsphere-jdbc实现atomikos-xa分布式事务

---
+++ distributed-transaction-atomikos-xa/src/main/java/com/nerotomato/xa/config/MybatisConfig.java
@@ -0,0 +1,30 @@
+package com.nerotomato.xa.config;
+
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by nero on 2021/5/31.
+ */
+@Configuration
+public class MybatisConfig {
+
+    @Bean(name = "SqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        MybatisSqlSessionFactoryBean mybatisSqlSessionFactoryBean = new MybatisSqlSessionFactoryBean();
+        mybatisSqlSessionFactoryBean.setDataSource(dataSource);
+        return mybatisSqlSessionFactoryBean.getObject();
+    }
+    @Bean(name = "SqlSessionTemplate")
+    public SqlSessionTemplate sqlSessionTemplate(@Qualifier("SqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
