59eef2c70f5061d0ed80c96dd5eba555b4465f8b
nerotomato <guanhaowen@outlook.com>
2021-11-21 15:59:50 +0800

    update

---
+++ 05-cache/springboot-cache/src/main/java/com/nerotomato/cache/mapper/UmsMemberMapper.java
@@ -0,0 +1,16 @@
+package com.nerotomato.cache.mapper;
+
+import com.nerotomato.cache.entity.UmsMember;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 会员表 Mapper 接口
+ * </p>
+ *
+ * @author nero
+ * @since 2021-06-18
+ */
+public interface UmsMemberMapper extends BaseMapper<UmsMember> {
+
+}
